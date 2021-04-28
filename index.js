18a9fa2fc085db2cdb788c70af2284546abedb38
mishel607 <mishel908@yahoo.com>
2021-04-28 15:11:14 +0600

    css added

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const morgan=require("morgan")
 
 
 const app=express();
+app.set('view engine','ejs')
 app.use(morgan("dev"))
 
+app.use(express.static(__dirname+'/public'));
+app.get('/views', function (req, res) {
+  res.sendFile(__dirname +'/home.ejs');
+})
 
-app.use("/",(req,res)=>{return res.send("hello !")})
-app.use("/home",(req,res)=>{return res.send("welcome to  node !!!")})
+
+app.use("/",(req,res)=>{return res.render('home')})
+
+
+app.use("/ho",(req,res)=>{return res.send("welcome to  node !!!")})
 app.use("/lag",(req,res)=>{return res.send("welcome to  lag !!!")})
 
 const PORT=process.env.PORT|| 8081;
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+    console.log(`Server is running on port localhost:${PORT}`);
+}) 
